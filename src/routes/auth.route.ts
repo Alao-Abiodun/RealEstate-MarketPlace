eda710ebc3b19207f86585922b36a3e823fb7974
Alao Abiodun <abiodundev@gmail.com>
2026-01-04 10:49:51 +0100

    feat: implement logic for upload and removing image from s3

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -2,6 +2,6 @@ import { Router } from 'express';
 import { forgotPassword, login } from '../controllers/auth.controller';
 
 export default (router: Router) => {
-    router.post('/login', login)
-    router.post('/forgot-password', forgotPassword)
+    router.post('/auth/login', login)
+    router.post('/auth/forgot-password', forgotPassword)
 }
--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import authRoute from './auth.route';
+import adRoute from './ad.route';
 
 const router = Router();
 
 authRoute(router);
+// adRoute(router);
 
 export default router;
