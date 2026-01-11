54e51e7108d93d79f9439b7c46a88417f6d22eda
Alao Abiodun <abiodundev@gmail.com>
2026-01-11 17:25:43 +0100

    feat: add endpoint to get user enquired ads

--- src/routes/ad.route.ts
+++ src/routes/ad.route.ts
@@ -9,7 +9,8 @@ import {
   removeAd,
   userAd,
   changeAdStatus,
-  contactAgent
+  contactAgent,
+  enquiredAds
 } from "../controllers/ad.controller";
 import { uploadFile } from "../utils/helpers/imageHandler.helper";
 import { userAuth } from "../middleware/authorization.middleware";
@@ -19,11 +20,12 @@ export default (router: Router) => {
   router.use(userAuth);
   router.post("/ad", createAdValidator, createAd);
   router.get('/ad/user', userAd);
+  router.post('/ad/contact-agent', contactAgent);
+  router.get('/ad/enquired-ad', enquiredAds);
   router.get('/ad/for-sell-and-rent/:actionType', adsForSellOrRent);
   router.put('/ad/:id', changeAd);
   router.delete('/ad/:id', removeAd);
   router.patch('/ad/:id/status', changeAdStatus);
-  router.post('/ad/contact-agent', contactAgent);
   router.get("/ad/:slug", fetchNearAd);
   router.post("/ad/upload-image", uploadFile.any(), uploadImage);
   router.delete("/ad/remove-image", uploadFile.any(), removeImage);
